dd1ac64b4d106fca74499043660c4ef34936df19
ruggz13 <nicolasruggieri7@gmail.com>
2019-09-19 09:16:28 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,38 @@
 # Enter your procedural solution here!
+# def findMultiples
+#     until i == 1000 do
+#         i+= 1
+#         array = []
+#         sum = 0
+#         if i % 3 || i % 5
+#             array.push(i)
+#         end
+#         array.each{|a| sum+=a}
+#     return sum   
+# end
+
+def collect_multiples(limit)
+    array = []
+    i = 0
+    while i < limit - 1
+        i += 1
+        if i % 3 == 0 || i % 5 == 0
+            array.push(i)
+        end
+    end
+    return array
+end
+
+def sum_multiples(limit)
+    array = []
+    i = 0
+    while i < limit - 1
+        i += 1
+        if i % 3 == 0 || i % 5 == 0
+            array.push(i)
+        end
+    end
+    sum = 0 
+    array.each{|a| sum+= a}
+    return sum
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,20 @@
 # Enter your object-oriented solution here!
+class Multiples
+
+    def initialize(limit)
+        @limit = limit
+    end
+
+    def collect_multiples
+        nums = Array(1...@limit)
+        nums.select{|num| (num % 3 == 0) || (num % 5 == 0)}
+    end
+
+    def sum_multiples
+        nums = Array(1...@limit)
+        sum = 0
+        nums = nums.select{|num| (num % 3 == 0) || (num % 5 == 0) }
+        nums.each{|num| sum+=num}
+        sum
+    end
+end
